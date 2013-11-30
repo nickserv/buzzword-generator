8a233babec21418a4b11ea647d9206735debe55a
Nicolas McCurdy <thenickperson@gmail.com>
2013-11-30 02:44:13 -0500

    Organize functions into two new Config and Generator modules

--- buzzword_generator.js
+++ buzzword_generator.js
@@ -1,50 +1,54 @@
 "use strict";
 
-function randomNumber(max) {
-  return Math.floor(Math.random() * max);
-}
-
-Array.prototype.random = function () {
-  return this[randomNumber(this.length)];
+var Config = {
+  convert: function (data) {
+    var categories = Object.keys(data);
+    return categories.reduce(function (memo, category) {
+      var convertedModifiers = data[category].map(function (modifier) {
+        return { value: modifier, category: category };
+      });
+      return memo.concat(convertedModifiers);
+    }, []);
+  }
 };
 
-function loadModifiers(data) {
-  var categories = Object.keys(data);
-  return categories.reduce(function (memo, category) {
-    var convertedModifiers = data[category].map(function (modifier) {
-      return { value: modifier, category: category };
-    });
-    return memo.concat(convertedModifiers);
-  }, []);
-}
+var Generator = {
+  decorate: function (modifiers, string) {
+    var modifier = Generator.randomFrom(modifiers);
+    switch (modifier.category) {
+    case 'before':
+      return modifier.value + ' ' + string;
+    case 'after':
+      return string + ' ' + modifier.value;
+    case 'prefixes':
+      return modifier.value + string;
+    case 'suffixes':
+      return string + modifier.value;
+    }
+  },
 
-function decorate(modifiers, string) {
-  var modifier = modifiers.random();
-  switch (modifier.category) {
-  case 'before':
-    return modifier.value + ' ' + string;
-  case 'after':
-    return string + ' ' + modifier.value;
-  case 'prefixes':
-    return modifier.value + string;
-  case 'suffixes':
-    return string + modifier.value;
-  }
-}
+  randomFrom: function (list) {
+    return list[Generator.randomNumber(list.length)];
+  },
 
-function swaggify(modifiers, string, probability) {
-  probability = probability || 1;
-  if (randomNumber(probability) === 0) {
-    string = swaggify(modifiers, decorate(modifiers, string), probability + 1);
+  randomNumber: function (max) {
+    return Math.floor(Math.random() * max);
+  },
+
+  swaggify: function (modifiers, string, probability) {
+    probability = probability || 1;
+    if (Generator.randomNumber(probability) === 0) {
+      string = Generator.swaggify(modifiers, Generator.decorate(modifiers, string), probability + 1);
+    }
+    return string;
   }
-  return string;
-}
+};
 
 $.getJSON('modifiers.json', function (data) {
-  var modifiers = loadModifiers(data);
+  var modifiers = Config.convert(data);
 
   function generate() {
-    $('.title').html(swaggify(modifiers, 'Scrum'));
+    $('.title').html(Generator.swaggify(modifiers, 'Scrum'));
   }
 
   $(document).ready(generate);

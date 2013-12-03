39b46e9ee023eda45f6b85fe9a59346f93e6488c
Nicolas McCurdy <thenickperson@gmail.com>
2013-12-03 05:00:10 -0500

    Don't redefine the text variable

--- generator.js
+++ generator.js
@@ -54,7 +54,7 @@ $.getJSON('config.json', function (rawConfig) {
     spaceKey = 32;
 
   function update(text) {
-    var text = text || Generator.generate(config);
+    text = text || Generator.generate(config);
 
     $('.title').html(text);
     history.replaceState({}, 'Buzzword Generator', encodeURI("/?text=" + text));
